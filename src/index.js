c043a39a9626c633bf0c4d9455ec6639d5329ac1
tkamanda <toelokemwa@gmail.com>
2024-07-23 02:42:53 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // getting reference to the form and task list 
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+
+//reference to the form and task list
+form.addEventListener('submit' ,(e) => {
+  //prevent the default form submission behaviour
+  e.preventDefault();
+
+  //Get the task description from the input field
+  const taskDescription = document.getElementById('new-task-description').value;
+
+  //check if the task description is not empty
+  if (taskDescription.trim() !== '') {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskDescription;
+
+    taskList.appendChild(taskItem);
+
+    form.reset();
+  }
 });
+});
+
